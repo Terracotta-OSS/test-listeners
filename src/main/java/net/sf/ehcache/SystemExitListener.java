36f0edd71bdceb749ef175de82bfcb6395f4dba2
cruise <cruise-unknown@terracottatech.com>
2013-08-16 23:34:57 +0000

    merge 41040 from trunk
    svn path=/projects/test-listeners/tags/release-1.0.1/; revision=41041

--- src/main/java/net/sf/ehcache/SystemExitListener.java
+++ src/main/java/net/sf/ehcache/SystemExitListener.java
@@ -7,79 +7,80 @@ import java.io.*;
 
 public class SystemExitListener extends RunListener {
 
-  public static String TARGET_FOLDER_LOCATION;
-  static {
-    String currentFolderPath = new File(".").getAbsolutePath();
-    int targetLastIndex = currentFolderPath.lastIndexOf("target");
-    if(targetLastIndex != -1) {
-      TARGET_FOLDER_LOCATION = currentFolderPath.substring(0,targetLastIndex) + File.separator + "target" + File.separator;
-    } else {
-      TARGET_FOLDER_LOCATION = new File("target").getAbsolutePath() + File.separator;
-    }
-  }
+	public static String TARGET_FOLDER_LOCATION;
+	static {
+		String currentFolderPath = new File(".").getAbsolutePath();
+		int targetLastIndex = currentFolderPath.lastIndexOf("target");
+		if (targetLastIndex != -1) {
+			TARGET_FOLDER_LOCATION = currentFolderPath.substring(0,
+					targetLastIndex)
+					+ File.separator
+					+ "target"
+					+ File.separator;
+		} else {
+			TARGET_FOLDER_LOCATION = new File("target").getAbsolutePath()
+					+ File.separator;
+		}
+	}
 
+	public static final String SYSTEM_EXIT_LISTENER_RESULT = TARGET_FOLDER_LOCATION
+			+ "surefire-reports" + File.separator;
 
-  public static final String SYSTEM_EXIT_LISTENER_RESULT = TARGET_FOLDER_LOCATION + "surefire-reports" + File.separator;
+	@Override
+	public void testStarted(Description description) throws Exception {
+		StringBuilder text = new StringBuilder();
+		String className = description.getClassName();
+		String descriptionDisplayName = description.getDisplayName();
+		String methodName = description.getMethodName();
+		text.append(
+				"-------------------------------------------------------------------------------\n")
+				.append("Test set: " + className + "\n")
+				.append("-------------------------------------------------------------------------------\n")
+				.append("Tests run: 1, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 0 sec <<< FAILURE! - in "
+						+ className + "\n")
+				.append(descriptionDisplayName
+						+ "  Time elapsed: 0.019 sec  <<< FAILURE!\n")
+				.append("This test crashed the VM running it, probably because of a System.exit()\n\n");
+		String fileNameTxt = SYSTEM_EXIT_LISTENER_RESULT + className + ".txt";
+		writeReport(text, fileNameTxt);
 
-  @Override
-  public void testStarted(Description description) throws Exception {
-    StringBuilder text =  new StringBuilder();
-    String className = description.getClassName();
-    String descriptionDisplayName = description.getDisplayName();
-    String methodName = description.getMethodName();
-    text
-            .append("-------------------------------------------------------------------------------\n")
-            .append("Test set: " + className + "\n")
-            .append("-------------------------------------------------------------------------------\n")
-            .append("Tests run: 1, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 0 sec <<< FAILURE! - in " + className + "\n")
-            .append(descriptionDisplayName + "  Time elapsed: 0.019 sec  <<< FAILURE!\n")
-            .append("This test crashed the VM running it, probably because of a System.exit()\n\n");
-    String fileNameTxt = SYSTEM_EXIT_LISTENER_RESULT + className + ".txt";
-    writeReport(text, fileNameTxt);
+		String fileNameXml = SYSTEM_EXIT_LISTENER_RESULT + "TEST-" + className
+				+ ".xml";
+		StringBuilder xml = new StringBuilder()
+				.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+				.append("<testsuite name=\""
+						+ className
+						+ "\" time=\"0\" tests=\"1\" errors=\"1\" skipped=\"0\" failures=\"0\">\n")
+				.append("  <testcase name=\"" + methodName + "\" classname=\""
+						+ className + "\" time=\"0.012\">\n")
+				.append("    <error message=\""
+						+ descriptionDisplayName
+						+ "\" type=\"System.exit\"><![CDATA[This test crashed the VM running it, probably because of a System.exit()]]></error>\n")
+				.append("    <system-err>This test crashed the VM running it, probably because of a System.exit().</system-err>\n")
+				.append("  </testcase>\n").append("</testsuite>");
+		writeReport(xml, fileNameXml);
+	}
 
-    String fileNameXml = SYSTEM_EXIT_LISTENER_RESULT + "TEST-" + className + ".xml";
-    StringBuilder xml = new StringBuilder()
-            .append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-            .append("<testsuite name=\"" + className + "\" time=\"0\" tests=\"1\" errors=\"1\" skipped=\"0\" failures=\"0\">\n")
-            .append("  <testcase name=\"" + methodName + "\" classname=\"" + className + "\" time=\"0.012\">\n")
-            .append("    <error message=\"" + descriptionDisplayName + "\" type=\"System.exit\"><![CDATA[This test crashed the VM running it, probably because of a System.exit()]]></error>\n")
-            .append("    <system-err>This test crashed the VM running it, probably because of a System.exit().</system-err>\n")
-            .append("  </testcase>\n")
-            .append("</testsuite>");
-    writeReport(xml, fileNameXml);
-  }
+	private void writeReport(StringBuilder sb, String fileName) {
+		try {
 
-  private void writeReport(StringBuilder sb, String fileName) {
-    try {
-
-      File file = new File(SYSTEM_EXIT_LISTENER_RESULT);
-      if (!file.exists()) {
-        file.mkdirs();
-      }
-      // we make sure we write to an empty file
-      File reportFile = new File(fileName);
-      if(reportFile.exists()) {
-        reportFile.delete();
-      }
-      PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
-      out.println(sb.toString());
-      out.close();
-    } catch (FileNotFoundException e) {
-      sb.append(e.getMessage());
-    } catch (IOException e) {
-      sb.append(e.getMessage());
-    }
-  }
-
-  @Override
-  public void testFinished(Description description) {
-    String className = description.getClassName();
-    String fileNameTxt = SYSTEM_EXIT_LISTENER_RESULT + className + ".txt";
-    new File(fileNameTxt).delete();
-    String fileNameXml = SYSTEM_EXIT_LISTENER_RESULT + "TEST-" + className + ".xml";
-    new File(fileNameXml).delete();
-
-  }
-
-
+			File file = new File(SYSTEM_EXIT_LISTENER_RESULT);
+			if (!file.exists()) {
+				file.mkdirs();
+			}
+			// we make sure we write to an empty file
+			File reportFile = new File(fileName);
+			if (reportFile.exists()) {
+				reportFile.delete();
+			}
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new FileWriter(fileName, true)));
+			out.println(sb.toString());
+			out.close();
+		} catch (FileNotFoundException e) {
+			sb.append(e.getMessage());
+		} catch (IOException e) {
+			sb.append(e.getMessage());
+		}
+	}
 }
